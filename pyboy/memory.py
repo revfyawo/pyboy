8ce37f7057d6fdbf93e1a11525f5b69caaded631
Jayjader <njay47@gmail.com>
2017-04-03 09:55:14 +0200

    Initial MBC commit.
    Tentative Class hierarchy established: Memory contains a Cartridge which
    may contain an MBC, depending on the Cartridge type.
    Muted "positional arguement" in list comprehensions as it is not needed.
    Updated Memory access to delegate to Cartridge when necessary.
    Added some error handling.

--- pyboy/memory.py
+++ pyboy/memory.py
@@ -1,8 +1,32 @@
+class Cartridge(object):
+    """ Game Cartridge
+    The first 32kB of address space is the game cartridge.
+    The first 16kB consist of ROM (aka the first ROM bank), and the following
+    16kB depend on the cartridge type. They can be just another 16kB of ROM space
+    (as in Tetris), or a dynamic load of further ROM banks (or even RAM).
+    """
+    def __init__(self):
+        """
+        This is minimalistic for testing/debugging purposes. The Cartridge object
+        should not be used before having loaded an actual game "rom" from file
+        """
+        self.rom0 = [0 for _ in range(0x7FFF + 1)]
+
+    def load_game_rom(rom_file_path):
+        with open(rom_file_path, 'rb') as rom:
+            pass
+            #todo: read byte by byte
+            
+
+class MBC(object):
+    """ Memory Bank Controller - Handles the extra ROM and/or RAM present in the cartridge """
+    pass
+
 class Memory(object):
     """" Memory """
     def __init__(self):
         self.iter_index = -1
-        self.mem = [0 for i in range(0xFFFF + 1)]
+        self.mem = [0 for _ in range(0xFFFF + 1)]
 
     def __iter__(self):
         self.iter_index = -1
@@ -14,9 +38,31 @@ class Memory(object):
             raise StopIteration
         return self.mem[self.iter_index]
 
-    def __getitem__(self, item):
-        return self.mem[item]
+    def __getitem__(self, key):
+        if key < 0 or key > 0xFFFF:
+            raise IndexError("Memory access out of bounds, address {} is invalid.".format(key))
+        elif key < 0x8000:
+            if self.cart is None:
+                raise CartridgeMissing
+            else:
+                return self.cart[key]
+        else:
+            return self.mem[key]
 
     def __setitem__(self, key, value):
-        self.mem[key] = value
+        if key < 0 or key > 0xFFFF:
+            raise IndexError("Memory access out of bounds, address {} is invalid.".format(key))
+        elif key < 0x8000:
+            if self.cart is None:
+                raise CartridgeMissing
+            else:
+                self.cart[key] = value
+        else:
+            self.mem[key] = value
         return value
+
+class CartridgeMissing(Exception):
+    """
+    As the name suggests, raised when trying to access the first
+    32kB of address space when the cartridge is not present/loaded
+    """
