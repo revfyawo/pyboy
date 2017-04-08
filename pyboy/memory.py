2cbd0a8f8ee1bdeea4195cdcf26ecdf11c0f81ab
Jayjader <njay47@gmail.com>
2017-04-09 00:36:11 +0200

    Merge branch 'master' into mbc_test
    # Conflicts:
    #	pyboy/memory.py

--- pyboy/memory.py
+++ pyboy/memory.py
@@ -1,8 +1,9 @@
 class Memory(object):
     """" Memory """
-    def __init__(self):
+    def __init__(self, cart=None):
         self.iter_index = -1
         self.mem = [0 for _ in range(0xFFFF + 1)]
+        self.cart = cart
 
     def __len__(self):
         return len(self.mem)
@@ -17,9 +18,34 @@ class Memory(object):
             raise StopIteration
         return self.mem[self.iter_index]
 
-    def __getitem__(self, item):
-        return self.mem[item]
+    def __getitem__(self, key):
+        if key < 0 or key > 0xFFFF:
+            raise IndexError("Memory access out of bounds, address {} is invalid.".format(key))
+        elif key < 0x8000 or (0xa000 <= key < 0xc000):
+            if self.cart is None:
+                # according to docs, return 0xff
+                raise CartridgeMissing
+            else:
+                return self.cart[key]
+        else:
+            return self.mem[key]
 
     def __setitem__(self, key, value):
-        self.mem[key] = value
-        return value
+        if key < 0 or key > 0xFFFF:
+            raise IndexError("Memory access out of bounds, address {} is invalid.".format(key))
+        elif key < 0x8000 or (0xa000 <= key < 0xc000):
+            if self.cart is None:
+                # according to docs, return 0xff
+                raise CartridgeMissing
+            else:
+                self.cart[key] = value
+        else:
+            self.mem[key] = value
+        # return value
+
+
+class CartridgeMissing(Exception):
+    """
+    As the name suggests, raised when trying to access the first
+    32kB of address space when the cartridge is not present/loaded
+    """
